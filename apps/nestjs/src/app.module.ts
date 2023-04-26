1aab3f9b3e057a641012e429e5b1a3fc3fc2abbb
Eduardo Breno <eduardobreno@msn.com>
2023-04-26 19:57:42 +0100

    wip: remove post entity

--- apps/nestjs/src/app.module.ts
+++ apps/nestjs/src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common'
-import { GraphQLModule } from '@nestjs/graphql'
-import { PrismaService } from './prisma.service'
-import { PostResolver } from './resolvers.post'
-import { UserResolver } from './resolvers.user'
-import { join } from 'path'
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaService } from './prisma.service';
+import { UserResolver } from './resolvers.user';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -15,6 +14,6 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
     }),
   ],
   controllers: [],
-  providers: [PrismaService, UserResolver, PostResolver],
+  providers: [PrismaService, UserResolver],
 })
 export class AppModule {}
